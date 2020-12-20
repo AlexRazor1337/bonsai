bc0c910e7344f1ed55ae3cfd018d18f5f13c19bd
AlexRazor1337 <alexrazor1337@gmail.com>
2020-12-20 10:56:42 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,86 @@
+#include <curses.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+#define BUFF_SIZE 256
+
+
+long double get_cpu_util() {
+    long double a[4], b[4], loadavg;
+    
+    FILE *stat = fopen("/proc/stat","r");
+    fscanf(stat, "%*s %Lf %Lf %Lf %Lf", &a[0], &a[1], &a[2], &a[3]);
+    fclose(stat);
+    sleep(1);
+    
+    stat = fopen("/proc/stat","r");
+    fscanf(stat, "%*s %Lf %Lf %Lf %Lf", &b[0], &b[1], &b[2], &b[3]);
+    fclose(stat);
+    
+    loadavg = ((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]));
+    
+    return loadavg;
+}
+
+
+void get_mem(int *avaiableRAM, int *totalRAM) {
+    FILE *meminfo;
+    meminfo = fopen("/proc/meminfo", "rt");
+    fscanf(meminfo, "%*s %d\n %*s %*s %*d %*s\n %*s %d", totalRAM, avaiableRAM);
+    
+    fclose(meminfo);
+}
+
+
+void get_time(char *buf) {
+    time_t rawtime;
+    
+    time(&rawtime);
+    strftime(buf, 32, "%a %d-%m %H:%M:%S", localtime(&rawtime));
+}
+
+
+int main() {
+    initscr();
+    start_color();
+    
+    curs_set(false);
+    init_color(COLOR_BLACK, 0, 0, 0);
+    
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    
+    attron(COLOR_PAIR(1));
+    bkgd(COLOR_PAIR(1));
+    
+    
+    FILE *file;
+    char buff[BUFF_SIZE];
+    char time_buff[32];
+    
+    int totalRAM, avaiableRAM;
+    
+    while (true) {
+        clear();
+        move(0, 0);
+        
+        get_mem(&avaiableRAM, &totalRAM);
+        get_time(time_buff);
+        
+        file = fopen("bs", "a+");
+        while (fgets(buff, BUFF_SIZE, file))
+            printw(buff, time_buff);
+        
+        fclose(file);
+        
+        printw("\n\tCPU Utilization: %.2Lf%%\n", get_cpu_util() * 100);
+        printw("\tUsed RAM: %d MB \n\tTotal RAM: %d MB (%.2f%% used)\n", totalRAM/1024 - avaiableRAM/1024, totalRAM/1024, ((double) (totalRAM/1024 - avaiableRAM/1024) / (totalRAM/1024)) * 100);
+        
+        refresh();
+    }
+    
+    return 0;
+}
